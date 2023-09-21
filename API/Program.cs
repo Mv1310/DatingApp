5b710fa8996aaffe4fc62b13198f4430d35ca575
Mv1310 <Maitri.Varia@rishabhsoft.com>
2023-09-21 10:39:26 +0530

    Adding Angular App

--- API/Program.cs
+++ API/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Options;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors();
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt => 
@@ -15,6 +16,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.UseHttpsRedirection();
 
